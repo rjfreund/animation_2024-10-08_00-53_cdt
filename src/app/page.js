ce63e584e0557482ce847e412ce05c306a17b899
R.J. Freund <robert.joseph.freund@gmail.com>
2024-10-08 02:00:31 -0500

    -

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,60 @@
-// pages/index.js
-import Link from 'next/link';
+"use client"
 
-export default function Home()
+// pages/animated.js
+import {useEffect,useRef} from 'react';
+import anime from 'animejs';
+import Image from 'next/image';
+
+const AnimatedPage=() =>
 {
-  return (
-    <div>
-      <h1>Welcome to the Next.js Anime.js Animation!</h1>
-      <Link href="/animated">
-        Go to Animated Page
-      </Link>
-    </div>
-  );
-}
+     const boxRef=useRef(null);
+
+     useEffect(() =>
+     {
+          // Create the animation when the component mounts
+
+          anime({
+               targets: boxRef.current,
+               translateY: 10,
+               //rotate: '1turn',
+               //backgroundColor: '#FF6347',
+               direction: 'alternate',
+               easing: 'easeInOutQuad',
+               duration: 2000,
+               loop: true
+          });
+     },[]);
+
+     return (
+          <div style={styles.container}>
+               <Image src="/OswinBackground.png" alt="Oswin  " width={0}
+                    height={0}
+                    sizes="100vw"
+                    style={{width: '100%',height: 'auto',position: 'absolute'}} />
+               <Image src="/Oswin.png" alt="Oswin  " width={0}
+                    height={0}
+                    sizes="100vw"
+                    style={{width: '100%',height: 'auto', position: 'absolute'}}
+                    ref={boxRef}/>
+
+          </div>
+     );
+};
+
+const styles={
+     container: {
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          width: '100vw',
+          backgroundColor: '#f0f0f0',
+     },
+     box: {
+          width: '100%',
+          height: '100%',
+          backgroundColor: '#00f',
+     },
+};
+
+export default AnimatedPage;
